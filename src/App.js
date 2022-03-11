d165ec4f93bdecf1baad6047eab1820263cf68e7
eudemonia <173537077@qq.com>
2022-03-11 18:12:12 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useEffect, useState } from 'react';
+
+const Test = () => {
+  const [test, setTest] = useState(1)
+  useEffect(() => {
+    setTest(1)
+    setTest(1)
+
+  }, [])
+
+  console.log(test,'hahahah')
+  return (
+    <div>
+      {test}
+      <div onClick={()=>setTest(1)}>12312312CLIKC</div>
+    </div>
+  )
+}
+
+class ClsComponent extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      text: '123'
+    }
+  }
+
+  // static getDerivedStateFromProps(props, state) {
+  //   console.log('getDerivedStateFormProps')
+  //   return {
+  //     text: props?.text
+  //   }
+  // }
+
+  componentDidCatch() {
+    console.log('componentDidCatch')
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount')
+    this.setState({ text: "componentDidMount" })
+    this.setState({ text: "123svss" })
+
+  }
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount')
+  }
+
+  getSnapshotBeforeUpdate(prePoprs, prestate) {
+    console.log('getSnapshotBeforeUpdate')
+    return null
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log('componentDidUpdate')
+  }
+
+
+
+  render() {
+    return (
+      <div>
+        ClsComponent{this.state.text}
+      </div>
+    )
+  }
+}
 
 function App() {
+  console.log('app')
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Test />
+      <ClsComponent />
+      1
     </div>
   );
 }
